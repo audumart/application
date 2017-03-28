6e5624b4f91e39a24f2e7b5f77b511c98700ea32
martin <audumart@gmail.com>
2017-03-28 11:09:39 +0100

    trying out pdo

--- www/test.php
+++ www/test.php
@@ -4,4 +4,15 @@ define('DBNAME', 'martin');
 define('DBUSER', 'root');
 define('DBPASS', 'root');
 
-$conn = new PDO('mysqli:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+try{
+
+	$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+} catch(PDOException $e){
+	echo $e->getMessage();
+}
+
+
+
+
