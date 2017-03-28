b6e2d991265ce375c3e2525a3fe4b7e639ad2d4a
martin <audumart@gmail.com>
2017-03-28 11:28:43 +0100

    saving changes

--- www/test.php
+++ www/test.php
@@ -5,9 +5,10 @@ define('DBUSER', 'root');
 define('DBPASS', 'root');
 
 try{
-
+	# prepare a pdo instance
 	$conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 
+	# set verbose error modes
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 } catch(PDOException $e){
 	echo $e->getMessage();
