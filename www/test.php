4799c051a3d5c64f24c60c4a5e864d9b28704bda
martin <audumart@gmail.com>
2017-03-28 11:53:41 +0100

    working on uploads

--- www/test.php
+++ www/test.php
@@ -1,6 +1,6 @@
-<?php
+<?php # test.php sandbox
 
-define('DBNAME', 'martin');
+/*define('DBNAME', 'martin');
 define('DBUSER', 'root');
 define('DBPASS', 'root');
 
@@ -12,7 +12,21 @@ try{
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
 } catch(PDOException $e){
 	echo $e->getMessage();
-}
+}*/
+
+	if (array_key_exists('save', $_POST)) {
+		print_r($_FILES);
+	}
+
+?>
+
+<form id="register" method="POST" enctype="multipart/form-data">
+	<p>please upload a file</p>
+	<input type="file" name="pic">
+
+	<input type="submit" name="save">
+
+	</form>
 
 
 
