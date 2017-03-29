7a0b046f3a25c3f1ebf44768e03a8fe6545c980b
martin <audumart@gmail.com>
2017-03-29 15:45:56 +0100

    getting errors to display properly

--- www/register.php
+++ www/register.php
@@ -48,24 +48,38 @@
 		<hr>
 		<form id="register"  action ="register.php" method ="POST">
 			<div>
+			<?php
+			if (isset($errors['fname'])) { echo '<span class="err">'.$errors['fname'].'</span>'; }
+			?>
 				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 			</div>
 			<div>
+			<?php
+			if (isset($errors['lname'])) { echo '<span class="err">'.$errors['lname'].'</span>'; }
+			?>
 				<label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
 			</div>
 
 			<div>
+			<?php
+			if (isset($errors['email'])) { echo '<span class="err">'.$errors['email'].'</span>'; }
+			?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+			<?php
+			if (isset($errors['password'])) { echo '<span class="err">'.$errors['password'].'</span>'; }
+			?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
  
-			<div>
+			<div><?php
+			if (isset($errors['pword'])) { echo '<span class="err">'.$errors['pword'].'</span>'; }
+			?>
 				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
 			</div>
