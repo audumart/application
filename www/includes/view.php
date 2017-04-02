c71d0e20b9e634f20580280e12a4c09781a1a4d4
martin <audumart@gmail.com>
2017-04-02 10:26:41 +0100

    adding view folder

---
+++ www/includes/view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $page_title ?></title>
+	<link rel="stylesheet" type="text/css" href="../styles/styles.css">
+</head>
+<body>
+	<section>
+		<div class="mast">
+			<h1><a href="home.php">T<span>SSB</span> Home </a></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="add_category.php" class="selected">Add Category</a></li>
+					<li><a href="#">View Category</a></li>
+					<li><a href="#">Logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+
+</body>
+</html>
