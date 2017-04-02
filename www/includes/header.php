8d51ed9bf3a562fd05b31cde3032bf6bbed3b506
martin <audumart@gmail.com>
2017-04-02 10:10:31 +0100

    removing some things from the includes

--- www/includes/header.php
+++ www/includes/header.php
@@ -11,22 +11,3 @@
 		</div>
 	</section>
 
-	<!DOCTYPE html>
-<html>
-<head>
-	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="styles/styles.css">
-</head>
-
-<body>
-	<section>
-		<div class="mast">
-			<h1>T<span>SSB</span></h1>
-			<nav>
-				<ul class="clearfix">
-					<li><a href="#" class="selected">add posts</a></li>
-					<li><a href="#">view posts</a></li>
-					<li><a href="#">logout</a></li>
-				</ul>
-			</nav>
-		</div>
