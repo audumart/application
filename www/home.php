5a38efc2e0d8a601709f63705722a378d3ba77c6
martin <audumart@gmail.com>
2017-04-02 19:02:15 +0100

    took out some feautures

--- www/home.php
+++ www/home.php
@@ -9,24 +9,6 @@
 
 	include 'includes/view.php';
 
-	$errors = [];
-
-	if (array_key_exists('enter', $_POST)) {
-		if (empty($_POST['cat'])) {
-			$errors['cat'] = "Enter Category name";
-		}
-	if (empty($errors)) {
-		// do database stuff
-
-		$clean = array_map('trim', $_POST);
-
-		insertCategory($conn, $clean);
-
-
-
-	}
-}
-
 
 
 
@@ -39,22 +21,13 @@
 	<title>Home</title>
 </head>
 <body>
-<div class="wrapper">
-		<div id="stream">
 
-		<form id="register" action="home.php" method="POST">
-
-			<label>Categories: </label>
-			<input type="text" name="cat" placeholder="Enter Product Category">
-
-			<input type="submit" name="enter" value="Enter">
 		
 
 		<div class="paginated">
 			<a href="#">1</a>
 			<a href="#">2</a>
 			<span>3</span>
-			<a href="#">2</a>
 		</div>
 	</div>
 	<?php
