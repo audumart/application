83dbcb15a45ac19143c847d0646e1d214debc9a8
martin <audumart@gmail.com>
2017-04-02 10:11:38 +0100

    adding admin home page

---
+++ www/home.php
@@ -0,0 +1,67 @@
+<?php
+	
+	session_start();
+	$page_title = "Home";
+
+	include 'includes/db.php';
+
+	include 'includes/functions.php';
+
+	include 'includes/view.php';
+
+	include 'includes/header.php';
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<div class="wrapper">
+		<div id="stream">
+
+		<form id="register" action="home.php" method="POST">
+			
+			<label>Category: </label>
+			<input type="text" name="cat" value="Enter Product Category">
+
+			<input type="submit" name="enter" value="Enter">
+		</form>
+			<table id="tab">
+				<thead>
+					<tr>
+						<th>post title</th>
+						<th>post author</th>
+						<th>date created</th>
+						<th>edit</th>
+						<th>delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>the knowledge gap</td>
+						<td>maja</td>
+						<td>January, 10</td>
+						<td><a href="#">edit</a></td>
+						<td><a href="#">delete</a></td>
+					</tr>
+          		</tbody>
+			</table>
+		</div>
+
+		<div class="paginated">
+			<a href="#">1</a>
+			<a href="#">2</a>
+			<span>3</span>
+			<a href="#">2</a>
+		</div>
+	</div>
+
+</body>
+</html>
