8a4ba0090649e29de0456d18378be7d0749863ec
martin <audumart@gmail.com>
2017-04-02 10:32:09 +0100

    modify footer

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -6,13 +6,6 @@
 </body>
 </html>
 
-<div class="paginated">
-			<a href="#">1</a>
-			<a href="#">2</a>
-			<span>3</span>
-			<a href="#">2</a>
-		</div>
-	</div>
 
 	<section class="foot">
 		<div>
