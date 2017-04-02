c23786c672efec9a08a61e3c228afbb0184005d1
martin <audumart@gmail.com>
2017-04-02 09:29:03 +0100

    adding footer for home page

--- www/includes/footer.php
+++ www/includes/footer.php
@@ -1,6 +1,22 @@
 <section class="foot">
 		<div>
-			<p>&copy; 2016;
+			<p>&copy; 2017;
+		</div>
+	</section>
+</body>
+</html>
+
+<div class="paginated">
+			<a href="#">1</a>
+			<a href="#">2</a>
+			<span>3</span>
+			<a href="#">2</a>
+		</div>
+	</div>
+
+	<section class="foot">
+		<div>
+			<p>&copy; 2017;
 		</div>
 	</section>
 </body>
