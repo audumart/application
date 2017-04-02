5a2071e758ced8d9a0c7f3acfc8c000b79e3e554
martin <audumart@gmail.com>
2017-04-02 18:11:08 +0100

    minor changes

--- www/home.php
+++ www/home.php
@@ -48,28 +48,7 @@
 			<input type="text" name="cat" placeholder="Enter Product Category">
 
 			<input type="submit" name="enter" value="Enter">
-		</form>
-			<table id="tab">
-				<thead>
-					<tr>
-						<th>post title</th>
-						<th>post author</th>
-						<th>date created</th>
-						<th>edit</th>
-						<th>delete</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>the knowledge gap</td>
-						<td>maja</td>
-						<td>January, 10</td>
-						<td><a href="#">edit</a></td>
-						<td><a href="#">delete</a></td>
-					</tr>
-          		</tbody>
-			</table>
-		</div>
+		
 
 		<div class="paginated">
 			<a href="#">1</a>
