4a7976d638a84a2d8800ba9d9c85b8a900095309
martin <audumart@gmail.com>
2017-04-04 03:57:05 +0100

    able to edit and delete products from view_product.php page now

--- www/view_product.php
+++ www/view_product.php
@@ -10,10 +10,52 @@ session_start();
 
 	include 'includes/view.php';
 
+	$errors = [];
+
+	
+	if (array_key_exists('edit', $_POST)) {
+		$clean = array_map('trim', $_POST);
+		editProduct($conn, $clean);
+	}
+	if (isset($_GET['success'])) {
+		echo $_GET['success'];
+	}
+
 
 ?>
 <div class="wrapper">
 		<div id="stream">
+			<p> 
+		<?php
+
+		if (isset($_GET['action'])) {
+			if ($_GET['action'] = "edit") {
+				
+			
+		
+
+
+
+		?>
+		<h3>Edit Product</h3>
+			<form id="register" method="POST" action="view_product.php">
+				<input type="text" name="title" placeholder="Book Name" value="<?php echo $_GET['title'];   ?>" />
+				<input type="hidden" name="book_id" value="<?php echo $_GET['book_id'];  ?>">
+				<input type="submit" name="edit" value="Edit">
+			</form>
+			<?php
+		}
+	}
+
+	if (isset($_GET['act'])) {
+		if ($_GET['act'] = "delete") {
+			deleteProduct($conn, $_GET['book_id']);
+
+		}
+	}
+
+			?>
+			</p>
 <h3>Available Products</h3>
 	<table id="tab">
 		<thead>
