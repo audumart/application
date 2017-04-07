2dd8d7452f778aaff271c469d08e7afce509301d
martin <audumart@gmail.com>
2017-04-07 22:42:56 +0100

    user login function working and registering users

--- www/reg.php
+++ www/reg.php
@@ -1,3 +1,75 @@
+<?php
+
+  include 'includes/db.php';
+
+  include 'includes/functions.php';
+
+  
+
+ 
+  $errors= [];
+
+ if (array_key_exists('register', $_POST)) {
+  # cache errors
+  
+
+
+  # validate first name
+  if (empty($_POST['fname'])) {
+    $errors['fname'] = "please enter a first name";
+  }
+  # validate last name
+  if (empty($_POST['lname'])) {
+    $errors['lname'] = "please enter a last name";
+  }
+  # validate email address
+  if (empty($_POST['email'])) {
+    $errors['email'] = "please enter an email address";
+
+  }
+  
+
+
+
+  # validate password
+  if (empty($_POST['password'])) {
+    $errors['password'] = "please enter a password";
+
+  }
+  # confirm password
+  if ($_POST['pword'] != $_POST['password']) {
+    $errors['pword'] = "passwords do not match";
+
+  }
+
+ 
+ if (empty($errors)) {
+  // do database stuff
+
+  # eliminate unwanted spaces from values in the $_POST array
+  $clean = array_map('trim', $_POST);
+
+  
+  doUserRegister($conn, $clean);
+  
+  
+
+     } 
+
+  
+}
+
+
+
+
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +83,7 @@
   <!-- top bar starts here -->
   <div class="top-bar">
     <div class="top-nav">
-      <a href="index.html"><h3 class="brand"><span>B</span>rain<span>F</span>ood</h3></a>
+      <a href="index.php"><h3 class="brand"><span>B</span>rain<span>F</span>ood</h3></a>
       <ul class="top-nav-list">
         <li class="top-nav-listItem Home"><a href="index.php">Home</a></li>
         <li class="top-nav-listItem catalogue"><a href="catalogue.php">Catalogue</a></li>
@@ -19,7 +91,7 @@
         <li class="top-nav-listItem register"><a href="reg.php">Register</a></li>
         <li class="top-nav-listItem cart">
           <div class="cart-item-indicator">
-            
+            <p>0</p>
           </div>
           <a href="cart.php">Cart</a>
         </li>
@@ -32,16 +104,23 @@
   <!-- main content starts here -->
   <div class="main">
     <div class="registration-form">
-      <form class="def-modal-form">
+      <form id="register" class="def-modal-form" action="reg.php" method="POST">
         <div class="cancel-icon close-form"></div>
         <label for="registration-from" class="header"><h3>User Registration</h3></label>
-        <input type="text" class="text-field first-name" placeholder="Firstname">
-        <input type="text" class="text-field last-name" placeholder="Lastname">
-        <input type="email" class="text-field email" placeholder="Email">
-        <input type="text" class="text-field username" placeholder="Username">
-        <input type="password" class="text-field password" placeholder="Password">
-        <input type="password" class="text-field confirm-password" placeholder="Confirm Password">
-        <input type="submit" class="def-button" value="Register">
+
+        <input type="text" name="fname" class="text-field first-name" placeholder="Firstname">
+
+        <input type="text" name="lname" class="text-field last-name" placeholder="Lastname">
+
+        <input type="email" name="email" class="text-field email" placeholder="Email">
+
+        <input type="text" name="username" class="text-field username" placeholder="Username">
+
+        <input type="password" name="password" class="text-field password" placeholder="Password">
+
+        <input type="password" name="pword" class="text-field confirm-password" placeholder="Confirm Password">
+
+        <input type="submit" name="register" class="def-button" value="Register">
         <p class="login-option">Have an account already? Login</p>
       </form>
     </div>
