b0e24edf41ffaf229e19afaadc9a48d067eb2b23
martin <audumart@gmail.com>
2017-04-08 20:33:46 +0100

    adding index page and getting top selling book to show

---
+++ www/index.php
@@ -0,0 +1,109 @@
+<?php
+  
+  include 'includes/db.php';
+
+
+  include 'includes/functions.php';
+
+  $show = showTop($conn);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>Home</title>
+</head>
+<body id="home">
+  <!-- DO NOT TAMPER WITH CLASS NAMES! -->
+
+  <!-- top bar starts here -->
+  <div class="top-bar">
+    <div class="top-nav">
+      <a href="index.php"><h3 class="brand"><span>B</span>rain<span>F</span>ood</h3></a>
+      <ul class="top-nav-list">
+        <li class="top-nav-listItem Home"><a href="index.php">Home</a></li>
+        <li class="top-nav-listItem catalogue"><a href="catalogue.php">Catalogue</a></li>
+        <li class="top-nav-listItem login"><a href="log.php">Login</a></li>
+        <li class="top-nav-listItem cart">
+          <div class="cart-item-indicator">
+            <p>12</p>
+          </div>
+          <a href="cart.php">Cart</a>
+        </li>
+      </ul>
+      <form class="search-brainfood">
+        <input type="text" class="text-field" placeholder="Search all books">
+      </form>
+    </div>
+  </div>
+  <!-- main content starts here -->
+  <div class="main">
+    <div class="book-display">
+      <div class="display-book" style="background: url('uploads/692837750hack5.jpeg');background-size: contain;background-position: center;background-repeat: no-repeat;"></div>
+      <div class="info">
+
+      <h2 class="book-title"><?php echo $show['title']; ?></h2>
+      <h2 class="book-title"><?php echo $show['author']; ?></h2>
+      <h2 class="book-title"><?php echo $show['price']; ?></h2>
+      
+
+        <form>
+          <label for="book-amout">Amount</label>
+          <input type="number" class="book-amount text-field">
+          <input class="def-button add-to-cart" type="submit" name="" value="Add to cart">
+        </form>
+      </div>
+    </div>
+    <div class="trending-books horizontal-book-list">
+      <h3 class="header">Trending</h3>
+      <ul class="book-list">
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$125</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$90</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$250</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$50</p></div>
+        </li>
+      </ul>
+    </div>
+    <div class="recently-viewed-books horizontal-book-list">
+      <h3 class="header">Recently Viewed</h3>
+      <ul class="book-list">
+        <div class="scroll-back"></div>
+        <div class="scroll-front"></div>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$250</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$50</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$125</p></div>
+        </li>
+        <li class="book">
+          <a href="#"><div class="book-cover"></div></a>
+          <div class="book-price"><p>$90</p></div>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
+  <!-- footer starts here-->
+  <div class="footer">
+    <p class="copyright">&copy; copyright 2017</p>
+  </div>
+</body>
+</html>
